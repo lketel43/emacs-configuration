f374537d2a5800921c971f745ed375afba0de855
Romain Bouarah <Rom.Bou@hotmail.fr>
2019-12-03 17:38:06 +0100

    Hide startup screen

--- init.el
+++ init.el
@@ -38,3 +38,16 @@ There are two things you can do about this warning:
   (load-theme 'zenburn t))
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (zenburn-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
